b74806d8ee2dd45517eb8cd797a49d246a4ec9b9
Alvaroskill <allinfer22@gmail.com>
2022-10-09 12:46:51 +0200

    Gestech

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,31 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'persons',
+    loadChildren: () => import('./pages/persons/persons.module').then( m => m.PersonsPageModule)
+  },
+  {
+    path: 'tasks',
+    loadChildren: () => import('./pages/tasks/tasks.module').then( m => m.TasksPageModule)
+  },
+  {
+    path: 'assign',
+    loadChildren: () => import('./pages/assign/assign.module').then( m => m.AssignPageModule)
+  },
+  {
+    path: 'viewtasks',
+    loadChildren: () => import('./pages/viewtasks/viewtasks.module').then( m => m.ViewtasksPageModule)
+  },
+
+
 ];
 
 @NgModule({
